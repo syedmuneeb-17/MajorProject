f5a910cd0630844e7da0c3b21aa10c5959f10211
syedmuneeb-17 <syedmuneebalirizvi17@gmail.com>
2024-11-26 21:20:19 +0500

    First Commit

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require("../models/user"); 
+
+module.exports.renderSignupForm =  (req,res) => {
+    res.render("users/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+    try{
+        let { username, email, password } = req.body;
+        const newUser = new User({ username, email });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to the TravelNest!");
+            res.redirect("/listings");
+        });
+    } catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup");    
+    }  
+};
+
+module.exports.renderLoginForm = (req,res) => {
+    res.render("users/login.ejs");
+};
+
+module.exports.login =  async(req, res) => {
+    req.flash("success", "Welcome Back to TravelNest!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res) => {
+    req.logout((err) => {
+        if (err) { 
+            return next(err); 
+        }
+        req.flash("success", "Goodbye!");
+        res.redirect("/listings");
+    });
+
+};
---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/travelnest";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({...obj, owner: "66ddfeb292c1e3d8f7204640"}));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
